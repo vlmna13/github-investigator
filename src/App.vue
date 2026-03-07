b05b209703f3373a96d1a7f33b71f5d734a81837
vlmna13 <vlmna13@gmail.com>
2026-03-07 20:39:47 +0300

    feat: add router and 404 page

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,12 @@
 <template>
   <AppHeader />
   <main class="container">
-    <SearchBar></SearchBar>
+    <RouterView />
   </main>
 </template>
 
 <script setup lang="ts">
 import AppHeader from './components/AppHeader.vue'
-import SearchBar from './components/SearchBar.vue';
 </script>
 
 <style scoped></style>
