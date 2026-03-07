693dc69a3b0ff6027b2aaeb4b563a88871a80da9
vlmna13 <vlmna13@gmail.com>
2026-03-07 19:49:37 +0300

    feat: add SearchBar component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
   <AppHeader />
+  <main class="container">
+    <SearchBar></SearchBar>
+  </main>
 </template>
 
 <script setup lang="ts">
 import AppHeader from './components/AppHeader.vue'
+import SearchBar from './components/SearchBar.vue';
 </script>
 
 <style scoped></style>
